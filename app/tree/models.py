cf9fde7a23279e8b919939c52b5db996b00fb1b5
Gabriel Sousa <gabriel@gsousa.com.br>
2023-02-14 14:54:12 -0300

    feat: ✨ create `Tree` related models
    - Tree model
    - PlantedTree model
    - Location model

--- app/tree/models.py
+++ app/tree/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+
+class PlantedTree(models.Model):
+    age = models.IntegerField()
+    planted_at = models.DateTimeField()
+    user = models.ForeignKey('core.user', on_delete=models.CASCADE)
+    account = models.ForeignKey('account.account', on_delete=models.CASCADE)
+    tree = models.ForeignKey('tree', on_delete=models.RESTRICT)
+
+    def save(self, *args, **kwargs):
+        latitude = kwargs.pop('latitude')
+        longitude = kwargs.pop('longitude')
+        super().save(*args, **kwargs)
+        new_location = Location.objects.create(
+            latitude=latitude,
+            longitude=longitude,
+        )
+        new_location.save(planted_tree=self)
+
+
+class Location(models.Model):
+    planted_tree = models.OneToOneField(
+        'plantedtree',
+        primary_key=True,
+        on_delete=models.CASCADE,
+    )
+    latitude = models.DecimalField(decimal_places=6, max_digits=12)
+    longitude = models.DecimalField(decimal_places=6, max_digits=12)
+
+
+class Tree(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    scientific_name = models.CharField(max_length=255, unique=True)
