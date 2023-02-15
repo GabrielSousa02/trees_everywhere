218995d3bc3895b776de3cc22799442b5b9fb213
Gabriel Sousa <gabriel@gsousa.com.br>
2023-02-14 23:41:35 -0300

    test: ✅ improve test for `Tree`

--- app/tree/tests/test_models.py
+++ app/tree/tests/test_models.py
@@ -5,12 +5,16 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 
 from tree.models import Tree
+from account.models import Account
 
 
 class ModelTests(TestCase):
     """Test Tree models."""
 
     def setUp(self):
+        self.account = Account.objects.create(
+            name='Sample Account 01'
+        )
         self.user = get_user_model().objects.create_user(
             email='user@example.com',
             password='testpass123',
@@ -21,6 +25,8 @@ class ModelTests(TestCase):
             name="Oak Tree",
             scientific_name="Quercus",
         )
+
+        self.account.add_member(self.user)
 
     def test_tree_create_sucess(self):
         """Test the successful creation of a tree."""
@@ -40,5 +46,6 @@ class ModelTests(TestCase):
         result = self.user.plant_tree(tree=tree_for_planting)
 
         self.assertTrue(result[0])
-        self.assertEqual(result[1].name, 'Oak Tree')
-        self.assertEqual(result[1].location, (-27.609895, -48.531859))
+        self.assertEqual(result[1].tree.name, 'Oak Tree')
+        self.assertEqual(result[1].location.latitude, -27.609895)
+        self.assertEqual(result[1].location.longitude, -48.531859)
