c7c95842ef5439d26e6813c97b7165bcdd8fdc0c
Gabriel Sousa <gabriel@gsousa.com.br>
2023-02-15 15:58:05 -0300

    feat: ✨ add admin page for `Tree` & `PlantedTree`

--- app/tree/admin.py
+++ app/tree/admin.py
@@ -1,3 +1,24 @@
+"""
+Django admin customization for Trees app.
+"""
 from django.contrib import admin
 
-# Register your models here.
+from tree import models
+
+
+class PlantedTreeInline(admin.TabularInline):
+    """Inline table for PlantedTrees for a Tree"""
+    model = models.PlantedTree
+    extra = 0
+
+    def has_change_permission(self, request, obj=None):
+        return False
+
+
+class TreeAdmin(admin.ModelAdmin):
+    """Define the admin page for trees."""
+    inlines = [PlantedTreeInline]
+
+
+admin.site.register(models.Tree, TreeAdmin)
+admin.site.register(models.PlantedTree)
